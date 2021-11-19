ce72631c663a31acf6b8be21c0ac92a4d5e524c4
kryban93 <kryban1993@gmail.com>
2021-11-19 21:07:18 +0100

    Updated ThemeToggler component

--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -2,18 +2,57 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
 	position: relative;
-	margin: 5px 20px;
+	display: flex;
+	box-sizing: border-box;
+	padding: 5px;
+	margin: 5px;
+	width: 80px;
+	height: 40px;
 `;
-const StyledOverlay = styled.label`
-	background-color: red;
+const StyledLabel = styled.label`
+	position: relative;
+	background-color: white;
+	width: 60px;
+	height: 29px;
+	border-radius: 50px;
+	cursor: pointer;
+	transition: background-color 0.2s ease-out;
+
+	&:checked {
+		background-color: blue;
+	}
+`;
+
+const StyledSpan = styled.span`
+	position: absolute;
+	content: '';
+	background-color: gray;
+	height: 25px;
+	width: 25px;
+	top: 2px;
+	left: 2px;
+	border-radius: 50px;
+	transition: left 0.4s ease-in, background-color 0.4s ease-in;
+`;
+
+const StyledInput = styled.input`
+	opacity: 0;
+	width: 0;
+	height: 0;
+
+	&:checked + ${StyledSpan} {
+		background-color: red;
+		left: 33px;
+	}
 `;
 
 const ThemeToggler = () => {
 	return (
 		<StyledWrapper>
-			<StyledOverlay htmlFor="themeToggler">
-				<input id="themeToggler" type="checkbox" />
-			</StyledOverlay>
+			<StyledLabel htmlFor="themeToggler">
+				<StyledInput id="themeToggler" type="checkbox" />
+				<StyledSpan />
+			</StyledLabel>
 		</StyledWrapper>
 	);
 };
